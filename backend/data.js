4a99a2b67dae718e0137a4d95c8ed211cf963995
dawon <lyumilove9@gmail.com>
2021-04-13 01:06:56 +0900

    express 이용 backend제작, MessageBox 및 LoadingBox 제작

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+// Product Data
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Denim Pants',
+      category: 'Pants',
+      image: '/images/product-1.jpg',
+      price: 20000,
+      countInStock: 10,
+      brand: 'Thinket',
+      rating: 5,
+      numReviews: 15,
+      description: 'high quality pants'
+    },
+    {
+      id: '2',
+      name: 'Flower Dress',
+      category: 'Dress',
+      image: '/images/product-2.jpg',
+      price: 25000,
+      countInStock: 20,
+      brand: 'Thinket',
+      rating: 4.5,
+      numReviews: 20,
+      description: 'high quality dress'
+    },
+    {
+      id: '3',
+      name: 'Flower Dress',
+      category: 'Dress',
+      image: '/images/product-2.jpg',
+      price: 28000,
+      countInStock: 0,
+      brand: 'Thinket',
+      rating: 4.0,
+      numReviews: 20,
+      description: 'high quality dress'
+    }
+  ]
+};
+
+export default data;
